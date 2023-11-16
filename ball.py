da17857287e703d2bc166afb492290f900d593d8
ccgarcia1 <42050096+ccgarcia1@users.noreply.github.com>
2023-11-15 22:35:38 -0300

    Final code Pong Game

--- ball.py
+++ ball.py
@@ -1,6 +1,5 @@
 from turtle import Turtle
 
-
 class Ball(Turtle):
 
     def __init__(self):
@@ -9,5 +8,26 @@ class Ball(Turtle):
         self.color("white")
         #self.shapesize(stretch_wid=5, stretch_len=1)
         self.penup()
+        self.x_move = 10
+        self.y_move = 10
+        self.move_speed = 0.1
+
+    def move(self):
+        new_x = self.xcor() + self.x_move
+        new_y = self.ycor() + self.y_move
+        self.goto(new_x, new_y)
+
+    def bounce(self, y_or_x):
+        self.move_speed *= 0.9
+        if y_or_x == 'Y':
+            self.y_move *= - 1
+        else:
+            self.x_move *= - 1
+
+    def reset_position(self):
         self.goto(0, 0)
+        self.move_speed = 0.1
+        self.bounce('X')
+
+
 
--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 from turtle import Screen, Turtle
 from paddle import Paddle
 from ball import Ball
+import time
+from scoreboard import Scoreboard
 
 
 
@@ -8,17 +10,46 @@ screen = Screen()
 screen.setup(width=800, height=600)
 screen.bgcolor("black")
 screen.title("Pong Game")
-#screen.tracer(0)
+screen.tracer(0)
+
 r_paddle = Paddle(position=350)
 l_paddle = Paddle(position=-350)
-
 ball = Ball()
+scoreboard = Scoreboard()
+
 
 screen.listen()
 screen.onkey(r_paddle.up, "Up")
 screen.onkey(r_paddle.down, "Down")
 screen.onkey(l_paddle.up, "w")
 screen.onkey(l_paddle.down, "a")
+
+game_is_on = True
+while game_is_on:
+    time.sleep(ball.move_speed)
+    screen.update()
+    ball.move()
+
+    #detect the collision with the wall
+    if ball.ycor() > 280 or ball.ycor() < -280:
+        ball.bounce('Y')
+
+    #detect collision with paddle
+    if ball.distance(r_paddle) < 50 and ball.xcor() > 320 or ball.distance(l_paddle) < 50 and ball.xcor() < -320:
+        ball.bounce('X')
+
+    #detect when the right paddle misses
+    if ball.xcor() > 390:
+        ball.reset_position()
+        scoreboard.summarize_points(l_or_r='L')
+        scoreboard.update_scoreboard()
+
+
+    #detect when the left paddle misses
+    if ball.xcor() < -390:
+        ball.reset_position()
+        scoreboard.summarize_points(l_or_r='R')
+        scoreboard.update_scoreboard()
 
 
 
